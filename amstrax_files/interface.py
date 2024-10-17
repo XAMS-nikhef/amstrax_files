a594e10ee9b0e4a1452456ada97598d39520ec62
Carlo Fuselli <cfuselli@nikhef.nl>
2024-10-17 20:41:06 +0200

    testing corrections

--- amstrax_files/interface.py
+++ amstrax_files/interface.py
@@ -5,11 +5,13 @@ from amstrax_files.utils import read_file
 
 export, __all__ = amstrax_files.exporter()
 
+GITHUB_RAW_URL = "https://raw.githubusercontent.com/XAMS-nikhef/amstrax_files/master/corrections/"
+
 
 @export
 def list_files():
     """Get a list of files we have we have in the fax files"""
-    p = _package_path('data')
+    p = _package_path("data")
     return _list_files(p)
 
 
@@ -20,23 +22,63 @@ def get_file(file_name):
     argument to read a specific format. otherwise we will assume it's in
     a text format. See straxen.get_recourse for more info.
     """
-    p = _package_path('data')
+    p = _package_path("data")
     return _get_file(file_name, get_from=p)
 
 
+# @export
+# def get_correction(file_name):
+#     """
+#     Get files stored under strax_files. Add "fmt = <format>" as an
+#     argument to read a specific format. otherwise we will assume it's in
+#     a text format. See straxen.get_recourse for more info.
+#     """
+#     p = _package_path("corrections")
+#     return _get_file(file_name, get_from=p)
+
+
 @export
-def get_abspath(file_name, sub_dir='data'):
+def get_correction(file_name):
+    """
+    Get correction file from GitHub or locally.
+    It first tries to download from GitHub, and if that fails, it fetches from the local corrections directory.
+    """
+    # Try to get the file from GitHub
+    try:
+        return _fetch_from_github(file_name)
+    except Exception as e:
+        print(f"Failed to fetch {file_name} from GitHub: {e}")
+        # Fallback to local if fetching from GitHub fails
+        print(f"Falling back to local corrections for {file_name}")
+        p = _package_path("corrections")
+        return _get_file(file_name, get_from=p)
+
+
+def _fetch_from_github(file_name):
+    """Fetch correction file from GitHub raw URL"""
+    url = GITHUB_RAW_URL + file_name
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # Successfully fetched the file, return its contents
+        return response.text
+    else:
+        raise FileNotFoundError(f"File {file_name} not found in GitHub repository")
+
+
+@export
+def get_abspath(file_name, sub_dir="data"):
     """Get the abspath of the file. Raise FileNotFoundError when not found in any subfolder"""
     p = os.path.join(_package_path(sub_dir), file_name)
     if os.path.exists(p):
         return p
-    raise FileNotFoundError(f'Cannot find {file_name}')
+    raise FileNotFoundError(f"Cannot find {file_name}")
 
 
 def _get_file(file_name, get_from):
     """Get a file from a subfolder in this package"""
     if file_name not in _list_files(get_from):
-        raise FileNotFoundError(f'No file {file_name} in {get_from}')
+        raise FileNotFoundError(f"No file {file_name} in {get_from}")
     else:
         path = os.path.join(get_from, file_name)
         return read_file(path)
@@ -44,7 +86,7 @@ def _get_file(file_name, get_from):
 
 def _package_path(sub_directory):
     """Get the abs path of the requested sub folder"""
-    return pkg_resources.resource_filename('amstrax_files', f'../{sub_directory}')
+    return pkg_resources.resource_filename("amstrax_files", f"../{sub_directory}")
 
 
 def _list_files(path):
